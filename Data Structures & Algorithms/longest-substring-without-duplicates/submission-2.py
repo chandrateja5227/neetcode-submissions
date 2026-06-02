439e0149e1cecffc14e0639db0b4f0892af94e18
Chandrateja Gollapelly <59494549+chandrateja5227@users.noreply.github.com>
2026-06-02 23:28:11 +0530

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        hash_index = defaultdict(int)
+        left = 0
+        right = 0
+        max_len = 0
+        while right < len(s):
+            if s[right] in hash_index:
+                next_index = hash_index.get(s[right])
+                while left < next_index + 1:
+                    del hash_index[s[left]]
+                    left+=1
+               
+                hash_index[s[right]] = right
+                max_len = max(max_len,len(hash_index))
+                right+=1
+            else:
+                hash_index[s[right]] = right
+                max_len = max(max_len,len(hash_index))
+                right+=1
+
+        return max_len
+
+
+
+
+
+
+
+        
